7744108174135ccc55cf819eaa12342298d1d60d
Beldaras <jphilippe.roblot@gmail.com>
2023-09-26 10:51:58 +0200

    prepare model to get current user's fav

--- src/model/fav.model.js
+++ src/model/fav.model.js
@@ -3,7 +3,8 @@ const database = require("./database.js");
 const findAll = async () => {
   try {
     const [favorites] = await database.query(
-      "SELECT * FROM favorites join user where user_id = user.id"
+      "SELECT * FROM favorites join user where user_id = ?",
+      [user_id] // test requête pour user courant
     );
     return favorites;
   } catch (error) {
@@ -13,8 +14,6 @@ const findAll = async () => {
 
 const addOne = async (favorite) => {
   try {
-    // const [user_id] = await database.query("SELECT id FROM user");
-    // console.log(user_id);
     const { city, lat, lon, user_id } = favorite;
     const [result] = await database.query(
       "INSERT INTO favorites (city, lat, lon, user_id) VALUES (?, ?, ?, ?)",
